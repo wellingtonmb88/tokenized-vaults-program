0fee8e3753716d60f6f03f10edea62b4010f3c32
Maicon Crespo <maiconviniciuscrespo@gmail.com>
2025-08-04 00:22:42 +0200

    feat(state): create ProtocolConfig state module
    Closes #1

---
+++ programs/tokenized-vaults-program/src/state/protocol_config.rs
@@ -0,0 +1,37 @@
+
+use crate::error::ErrorCode;
+use anchor_lang::prelude::*;
+
+pub const PROTOCOL_CONFIG_SEED: &str = "protocol_config";
+
+#[account]
+#[derive(Default, Debug)]
+pub struct ProtocolConfig{
+    pub admin_authority: Pubkey,
+    pub protocol_fees:u64,
+    pub status:u8,
+    pub bump:u8,
+}
+
+impl ProtocolConfig{
+    pub const LEN: usize = 8+ // discriminator
+        32 + // owner
+        8 + // protocol_fees
+        1 + // status
+        1 ;// bump
+       
+    
+         pub fn is_authorized<'info>(
+        &self,
+        signer: &Signer<'info>,
+        expect_pubkey: Pubkey,
+    ) -> Result<()> {
+        require!(
+            signer.key() == self.admin_authority || expect_pubkey == signer.key(),
+            ErrorCode::NotApproved
+        );
+        Ok(())
+    }
+
+}
+
