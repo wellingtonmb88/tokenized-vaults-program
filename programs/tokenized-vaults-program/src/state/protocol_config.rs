4641a8f69af4eaa75829ff3d51a6a84f19a2cf34
Maicon Crespo <maiconviniciuscrespo@gmail.com>
2025-08-06 02:01:15 +0200

    fix(instructions): fix Protocol Admin instruction module
    Closes #2

--- programs/tokenized-vaults-program/src/state/protocol_config.rs
+++ programs/tokenized-vaults-program/src/state/protocol_config.rs
@@ -4,6 +4,10 @@ use anchor_lang::prelude::*;
 use core::mem::size_of;
 
 pub const PROTOCOL_CONFIG_SEED: &str = "protocol_config";
+/// Scale: 1% = 10_000 bps → 100% = 1_000_000
+pub const BPS: u64 = 10_000;
+/// High fee for transaction: 10%
+pub const HIGH_FEES: u64 = 10 * BPS; // 100_000
 
 #[derive(Default, Debug)]
 #[account(discriminator = 1)]
