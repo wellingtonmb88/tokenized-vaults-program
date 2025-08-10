68b27b9afe6750cd91a36a9d2ebf68eccf659d85
rafaelzochling <rafaelzochling@gmail.com>
2025-08-11 00:05:24 +0100

    feat: adjust features to have different pubkeys for devnet and mainnet

--- programs/tokenized-vaults-program/src/constants.rs
+++ programs/tokenized-vaults-program/src/constants.rs
@@ -9,6 +9,11 @@ pub const HIGH_PERFORMANCE_FEE: u32 = 100u32.checked_mul(BPS).unwrap(); // 1_000
 /// Low performance fee: 10%
 pub const LOW_PERFORMANCE_FEE: u32 = 10u32.checked_mul(BPS).unwrap(); // 100_000
 
-/// USDC mint address on Solana mainnet
-/// https://solscan.io/token/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v
-pub const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
+/// USDC mint address - conditional based on network
+/// Devnet: Test token that can be minted for testing
+/// Mainnet: Official USDC token (EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v)
+#[cfg(feature = "devnet")]
+pub const USDC_MINT: Pubkey = pubkey!("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU"); // USDC-Dev on devnet
+
+#[cfg(not(feature = "devnet"))]
+pub const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"); // USDC on mainnet
