ec41dd3a9de9bfd64844a4c0029484fc57aedab1
rafaelzochling <rafaelzochling@gmail.com>
2025-08-13 17:46:23 +0100

    feat: #23 - adjust vlt_strtg_cfg_vault seed as per PR comment

--- programs/tokenized-vaults-program/src/state/vault_strategy_config.rs
+++ programs/tokenized-vaults-program/src/state/vault_strategy_config.rs
@@ -23,8 +23,8 @@ pub struct VaultStrategyConfig {
 }
 
 impl VaultStrategyConfig {
-    pub const SEED: &str = "vault_strategy_config:";
-    pub const VAULT_SEED: &str = "vault_strategy_config_vault";
+    pub const SEED: &str = "vlt_strategy_config:";
+    pub const VAULT_SEED: &str = "vlt_strtg_cfg_vault";
 
     pub fn initialize(
         &mut self,
